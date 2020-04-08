d7f486a5323bd7d95bef197f7602ee37be32ff95
Elena Schuler <eschuler@Mathilda.local>
2020-04-08 20:59:32 +0200

    teames 2 TEAMNAME

--- runmerunner/src/main/java/htwb/ai/MyTest.java
+++ runmerunner/src/main/java/htwb/ai/MyTest.java
@@ -1,13 +1,2 @@
 package htwb.ai;
 
-import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.Target;
-
-@Retention(RUNTIME)
-@Target(METHOD)
-public @interface MyTest {
-
-}
